86c62b2b1069913c459ed1eb1f1e1f9c67e11d88
ucyo <cayoglu@me.com>
2023-03-30 08:59:42 +0200

    Add scaffolding for cli tool
    - Use argparse for argument parsing since it is included in std

--- wetter/tests/test_conn.py
+++ wetter/tests/test_conn.py
@@ -1,9 +1,11 @@
 from wetter import conn
 import pandas as pd
 
+
 def test_connection_returns_df():
     data = conn.get_db()
     assert isinstance(data, pd.DataFrame)
+
 
 def test_db_has_no_empty_values():
     data = conn.get_db()
