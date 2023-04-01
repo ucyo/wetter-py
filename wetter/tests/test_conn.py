d37dc9284ff5b756f073d5aeadd673ff046ad559
ucyo <cayoglu@me.com>
2023-04-01 23:16:20 +0200

    Restructuring of back end for interface objects
    - There has been a major restructuring of the backend system. What has
      been introduced with this change is the common `WetterDB` class for
      accessing the data in the backend and the `QueryTicket` for updating
      the database.
    - The goal for `WetterDB` allows for the db to be represented in a
      single data class. While this was also true for the `pd.DataFrame`
      used before, but with the current solution one can also make
      guidelines on how the data needs to be saved on disk and how the
      fields are named.
    - The second class `QueryTicket` allows for pre-processing of the query
      requests send to the external APIs. These might include sorting of the
      dates or restricting the time distance between start and end date.

--- wetter/tests/test_conn.py
+++ wetter/tests/test_conn.py
@@ -1,21 +1,53 @@
-from wetter import conn
+import os
+from datetime import datetime as dt
+
 import pandas as pd
-from datetime import datetime
+import pytest
+
+from wetter import conn
 
 
-def test_connection_returns_df():
-    data = conn.get_db()
-    assert isinstance(data, pd.DataFrame)
+@pytest.fixture
+def db():
+    db = conn.get_db()
+    yield db.df
 
 
-def test_correct_namin():
-    data = conn.get_db()
-    assert "temperature" in data.columns
-    assert "wind" in data.columns
-    assert "wmo" in data.columns
-    assert isinstance(data.index[0], datetime)
+def test_connection_returns_df(db):
+    assert isinstance(db, pd.DataFrame)
 
 
-def test_db_has_no_empty_values():
-    data = conn.get_db()
-    assert data.isna()._values.sum() == 0
+def test_correct_naming(db):
+    assert "temperature" in db.columns
+    assert "wind" in db.columns
+    assert isinstance(db.index[0], dt)
+
+
+def test_db_has_no_empty_values(db):
+    assert db.isna()._values.sum() == 0
+
+
+@pytest.mark.web
+def test_update():
+    filename = os.path.join(conn.DATA_LOCATION, "test.json")
+    old = conn.WetterDB(filename).df
+    new = conn.WetterDB.update(filename, dry_run=True)
+    assert new.size >= old.size
+
+
+@pytest.mark.web
+def test_to_old():
+    start = dt(year=1201, month=1, day=1)
+    end = dt.now()
+    qt = conn.QueryTicket(start=start, end=end)
+
+    with pytest.raises(Exception):
+        conn.OpenMeteoMeasurements.get(qt)
+
+
+def test_query_ticket_reversed_dates():
+    end = dt.now()
+    start = dt.now()
+    qt = conn.QueryTicket(start=start, end=end)
+    assert qt.end == start
+    assert qt.start == end
--- wetter/wetter/conn.py
+++ wetter/wetter/conn.py
@@ -1,22 +1,124 @@
-import pandas as pd
-from datetime import datetime
+import json
 import os
+from dataclasses import dataclass
+from datetime import datetime as dt
+from datetime import timezone as tz
+
+import numpy as np
+import pandas as pd
+import requests as rqs
+from pytz import UTC
 
 DATA_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data")
-DB = os.path.join(DATA_LOCATION, "OpenMeteo.json")
+DB = os.path.join(DATA_LOCATION, "data.json")
 
 
 def get_db():
-    _raw_data = pd.read_json(DB)
-    df = pd.DataFrame(
-        {
-            "time": [
-                datetime.strptime(x, "%Y-%m-%dT%H:%M") for x in _raw_data.hourly["time"]
-            ],
-            "temperature": _raw_data.hourly["temperature_2m"],
-            "wind": _raw_data.hourly["windspeed_10m"],
-            "wmo": _raw_data.hourly["weathercode"],
-        }
-    )
-    df = df.set_index("time")
-    return df
+    return WetterDB(DB)
+
+
+class WetterDB:
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            self._raw_data = json.load(f)
+        number_of_measurements = len(self._raw_data["data"])
+        measurements = {self._raw_data["index"][i]: self._raw_data["data"][i] for i in range(0, number_of_measurements)}
+        df = pd.DataFrame({"time": self._raw_data["columns"], **measurements})
+        df = df.set_index("time")
+        df.index = df.index.astype(np.datetime64).tz_localize("UTC")
+        self.df = df
+        self.check_df()
+
+    def check_df(self):
+        assert "temperature" in self.df.columns
+        assert "wind" in self.df.columns
+        assert self.df.ndim == 2
+        assert self.df.shape[1] == 2
+        assert self.df.index.size > 0
+        assert self.df.index[0].tzinfo == UTC
+
+    def __getattr__(self, name):
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            return getattr(self.df, name)
+
+    @staticmethod
+    def update(db_location, dry_run=False):
+        base = WetterDB(db_location)
+        latest = base.df.index.max()
+        now = dt.utcnow().astimezone(tz.utc)
+        qt = QueryTicket(start=latest, end=now)
+        new_measurements = OpenMeteoMeasurements.get(qt)
+        df = pd.concat([base.df, new_measurements])
+        df = df[~df.index.duplicated(keep="last")]
+        if not dry_run:
+            df.T.to_json(db_location, orient="split", date_format="iso")
+        return df
+
+
+class OpenMeteoMeasurements:
+    @staticmethod
+    def parse(response):
+        df = pd.DataFrame(
+            {
+                "time": [dt.strptime(x, "%Y-%m-%dT%H:%M").astimezone(tz=tz.utc) for x in response["hourly"]["time"]],
+                "temperature": response["hourly"]["temperature_2m"],
+                "wind": response["hourly"]["windspeed_10m"],
+            }
+        )
+        df = df.set_index("time")
+        return df
+
+    @staticmethod
+    def url():
+        return (
+            "https://api.open-meteo.com/v1/forecast?"
+            + "latitude={lat:.2f}&longitude={lon:.2f}&"
+            + "timezone={tz}&start_date={start}&end_date={end}&"
+            + "hourly=temperature_2m,windspeed_10m,weathercode&current_weather=true"
+        )
+
+    @staticmethod
+    def archive():
+        return (
+            "https://archive-api.open-meteo.com/v1/archive?"
+            + "latitude={lat:.2f}&longitude={lon:.2f}&"
+            + "timezone={tz}&start_date={start}&end_date={end}&"
+            + "hourly=temperature_2m,windspeed_10m"
+        )
+
+    @staticmethod
+    def get(qt):
+        assert isinstance(qt, QueryTicket)
+        url = OpenMeteoMeasurements.url()
+        api = url.format(
+            lat=qt.lat,
+            lon=qt.lon,
+            tz=qt.tz,
+            start=qt.start.strftime("%Y-%m-%d"),
+            end=qt.end.strftime("%Y-%m-%d"),
+        )
+        resp = rqs.get(api)
+        if resp.status_code == 200:
+            json = resp.json()
+            df = OpenMeteoMeasurements.parse(json)
+            df = df[df.index <= qt.end]  # kick out predictions
+        else:
+            raise Exception(f"An error occurred during request [{resp.status_code}]: {resp.json()}")
+        return df
+
+
+@dataclass
+class QueryTicket:
+    start: dt.date
+    end: dt.date
+    lat: float = 49
+    lon: float = 8.41
+    tz: str = "UTC"
+
+    def __post_init__(self):
+        if self.start > self.end:
+            tmp = self.start
+            self.start = self.end
+            self.end = tmp
